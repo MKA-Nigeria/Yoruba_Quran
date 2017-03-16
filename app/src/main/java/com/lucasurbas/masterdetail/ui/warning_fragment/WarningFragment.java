de0a0dd3f23b501cc6d0bd6e1635b16961d3b3c3
Abdul-Mujib Aliu <abdulmujibaliu@MacBook-Pro.local>
2017-03-16 14:33:07 +0100

    New commit at 2:33PM ... Anthony deal just came in! Happy AF ... Time for zuhr tho..So Orevour

---
+++ app/src/main/java/com/lucasurbas/masterdetail/ui/warning_fragment/WarningFragment.java
@@ -0,0 +1,55 @@
+package com.lucasurbas.masterdetail.ui.warning_fragment;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lucasurbas.masterdetail.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by abdulmujibaliu on 3/16/17.
+ */
+
+public class WarningFragment extends DialogFragment {
+
+
+    private Activity mContext;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        dialog.setContentView(R.layout.data_warning_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        ButterKnife.bind(getActivity());
+        mContext = getActivity();
+
+        TextView mTitleText = (TextView) dialog.findViewById(R.id.title);
+        TextView mDescriptionText = (TextView) dialog.findViewById(R.id.message_content);
+        TextView mOKayButton = (TextView) dialog.findViewById(R.id.ok_button);
+        ImageView mBackImage = (ImageView) dialog.findViewById(R.id.back_ground_dialog);
+
+        mTitleText.setText("Large Download");
+        mDescriptionText.setText("This app requires that you download some data files of about 200MB, it is recoomended that you continue on an unmettered wifi connection");
+        mOKayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+        return dialog;
+    }
+}
+
